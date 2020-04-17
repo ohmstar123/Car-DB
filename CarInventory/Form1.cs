d1cc1b56731332e5d25b15ceef548aab9a277dec
Ohm Patel <ohmpate114@ed.amdsb.ca>
2020-04-16 22:03:38 -0400

    works the first time I exit the application, but when I try the second time, it doesn't work properly

--- CarInventory/Form1.cs
+++ CarInventory/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
+
 
 namespace CarInventory
 {
@@ -16,6 +18,7 @@ namespace CarInventory
         public Form1()
         {
             InitializeComponent();
+            loadDB();
         }
 
         private void addButton_Click(object sender, EventArgs e)
@@ -45,6 +48,75 @@ namespace CarInventory
                     + c.colour + " "
                     + c.mileage + "\n";
             }
+        }
+
+        public void loadDB()
+        {
+            string year, make, colour, mileage;
+
+            XmlReader reader = XmlReader.Create("Resources/CarData.xml");
+
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Text)
+                {
+                    reader.ReadToNextSibling("year");
+                    year = reader.ReadString();
+
+                    reader.ReadToNextSibling("make");
+                    make = reader.ReadString();
+
+                    reader.ReadToNextSibling("colour");
+                    colour = reader.ReadString();
+                    
+                    reader.ReadToNextSibling("mileage");
+                    mileage = reader.ReadString();
+
+                    Car newCar = new Car(year, make, colour, mileage);
+                    carsList.Add(newCar);
+
+
+                }
+            }
+
+            reader.Close();
+        }
+
+        public void saveDB()
+        {
+            XmlWriter writer = XmlWriter.Create("Resources/CarData.xml", null);
+
+            writer.WriteStartElement("Car");
+
+            foreach (Car e in carsList)
+            {
+                writer.WriteStartElement("Car");
+
+                writer.WriteElementString("year", e.year);
+                writer.WriteElementString("make", e.make);
+                writer.WriteElementString("colour", e.colour);
+                writer.WriteElementString("mileage", e.mileage);
+
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndElement();
+
+            writer.Close();
+        }
+
+        private void ClearLabels()
+        {
+            yearInput.Text = "";
+            makeInput.Text = "";
+            colourInput.Text = "";
+            mileageInput.Text = "";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            saveDB();
+            Application.Exit();
         }
     }
 }
